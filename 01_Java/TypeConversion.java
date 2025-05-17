fe86d37c8124a9bba76531309a43efcbe86b8bb9
mdsahil009 <mohdsahil00736@gmail.com>
2025-05-17 10:53:04 +0530

    add type Casting

---
+++ 01_Java/TypeConversion.java
@@ -0,0 +1,25 @@
+public class TypeConversion {
+
+    public static void main(String[] args) {
+
+        // implict conversion of data types
+        float myfloat = 520;
+        System.out.println(myfloat);
+        long mylong = 42;
+        System.out.println(mylong);
+        double mydouble = 2300.145f;
+        System.out.println(mydouble);
+
+        //  explict conversion of data types 
+
+        int myint = (int) 12.23;
+        System.out.println(myint);
+
+        float anothflot = (float) 5.06f;
+        System.out.println(anothflot);
+
+        long anothlong = (long) 3.4;
+        System.out.println(anothlong);
+
+    }
+}
