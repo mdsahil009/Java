7fdc9e1064b7e9b51f4983264396ae802b47f53e
mdsahil009 <mohdsahil00736@gmail.com>
2025-05-22 22:01:58 +0530

    adding bitwise operation od compliment

---
+++ 01_Java/bitwiseCompliment.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bitwiseCompliment {
+    public static void main(String[]  args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bit wise Operation of addition : ");
+
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+
+        int result = ~ num; 
+        System.out.println("Bitwise Compliment : " + result); 
+}
+}
