455c4fba2b6de49533f3b378015a06e7c48a6acb
mdsahil009 <mohdsahil00736@gmail.com>
2025-05-23 21:22:32 +0530

    adding bitwise XOR Operation for two number

---
+++ 01_Java/bitwiseXOR.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bitwiseXOR {
+    public static void main(String[]  args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bit wise Operation of addition : ");
+
+        System.out.print("Enter the number : ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the another number : ");
+        int num2  = input.nextInt();
+
+        int sum = num1 ^ num2;
+        System.out.print("Bitwise Operation of XOR : " + sum);
+    }
+    
+}
